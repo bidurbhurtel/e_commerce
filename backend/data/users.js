06908b6302c31d6af4a5a26eae29968c7a00316c
bidurbhurtel <bidurbhurtel02@gmail.com>
2021-01-09 10:13:30 -0600

    redux implementation problem solved

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,23 +1,22 @@
 import bcrypt from 'bcryptjs'
-//import User from '../models/userModel.js'
 
 const users = [
-    {
-        name: 'Admin User',
-        email: 'admin@example.com',
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: true
-    },
-    {
-        name: 'Bidur Bhurtel',
-        email: 'bidurbhurtel@exampple.com',
-        password: bcrypt.hashSync('123456', 10),       
-    },
-    {
-        name: 'Anusha Bhattarai',
-        email: 'anushabhattarai@example.com',
-        password: bcrypt.hashSync('123456', 10),        
-    }
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'Jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
 ]
 
 export default users
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -9,16 +9,16 @@ import ProductScreen from './screens/ProductScreen'
 const App = () => {
   return (
     <Router>
-      <Header/>
+      <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact/>
-          <Route path='/product/:id' component={ProductScreen} exact/>
-        </Container>        
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
       </main>
-      <Footer/>
+      <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
